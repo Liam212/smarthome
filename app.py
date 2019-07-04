95ca044ef67a2862d3274eecbb57d7350d70326e
liam212 <liamstout212@gmail.com>
2019-07-04 02:20:07 +0100

    Fixed color request

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-class DeviceList(Resource):
+class deviceState(Resource):
     def get(self):
         subprocess.call('node modules/magichome/app.js', shell=True)
         return 200
 
-@app.route('/color',methods = ['POST'])
-def color():
-    with open("modules/magichome/color.json", 'w') as file:
-        rgb = [str(request.form["r"]), str(request.form["g"]), str(request.form["b"])]
-        json.dump(rgb, file)
-        #subprocess.call('node modules/magichome/color.js', shell=True)
-    return '200'
+class deviceColor(Resource):
+    def post(self):
+        with open("modules/magichome/color.json", 'w') as file:
+            rgb = {"r":request.form["r"],"g":request.form["g"],"b":request.form["b"]}
+            json.dump(rgb, file, indent=4)
+            file.flush()
+            subprocess.call('node modules/magichome/color.js', shell=True)
+            return 200
 
-api.add_resource(DeviceList, '/')
+api.add_resource(deviceState, '/')
+api.add_resource(deviceColor, '/color')
 
 if __name__ == '__main__':
     app.run(debug=True)
