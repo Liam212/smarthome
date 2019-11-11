9a904bcd6967a4ec07023f5e06f57e14ef9009cb
Liam Stout <Liamstout212@gmail.com>
2019-11-11 11:18:49 +0000

    Updatded to support new version of api

--- smart-home-ui/src/components/FetchApi.js
+++ smart-home-ui/src/components/FetchApi.js
@@ -17,7 +17,6 @@ class FetchApi extends React.Component {
         }
   
       async componentDidMount() {
-        
         this.fetchState()  
         this.interval = setInterval(
           () =>
@@ -31,40 +30,49 @@ class FetchApi extends React.Component {
         const url = "http://localhost:8081/api/state"
         const response = await fetch(url);
         const data = await response.json();
-        data.devices.map(async device => {
-          this.setState({
-            [device.id]: device
-          });
-        });
-        this.setState({ loading: false })
+        this.setState({ loading: false, devices: data })
       }
 
       async updateState(deviceId) {
-        const url = "http://localhost:8081/api/power/"+(deviceId)
-        const response = await fetch(url);
-        const data = await response.json();
-        if (data.code === "200") {
-          NotificationManager.success(data.message);
-          this.setState({timer: 0});
-          if (deviceId === "all") {
-            this.fetchState()
-          } else {
-              this.state[deviceId].state = !this.state[deviceId].state;
+        if (deviceId === 'all') {
+          for (let index = 0; index < this.state.devices.length; index++) {
+            const url = "http://localhost:8081/api/power/"+(index)
+            const response = await fetch(url);
+            const data = await response.json();
+            if (data === true) {
+              this.state.devices[index].device.on = !this.state.devices[index].device.on;
+              NotificationManager.success("Device updated");
+              this.setState({timer: 0});
+            } else {
+              NotificationManager.error(data.message);
+            }
           }
-        } else {
-          NotificationManager.error(data.message);
-        }
+          } else {
+            const url = "http://localhost:8081/api/power/"+(deviceId)
+            const response = await fetch(url);
+            const data = await response.json();
+            if (data === true) {
+              this.state.devices[deviceId].device.on = !this.state.devices[deviceId].device.on;
+              NotificationManager.success("Device updated");
+              this.setState({timer: 0});
+            } else {
+              NotificationManager.error(data.message);
+            }
+          }
       }
+  
 
       async updateColor(color) {
-        const url = "http://localhost:8081/api/color/all"+"/"+(color.r)+","+(color.g)+","+(color.b)
-        const response = await fetch(url);
-        const data = await response.json();
-        if (data.code === "200") {
-          NotificationManager.success(data.message);
-          this.setState({timer: 0});
-        } else {
-          NotificationManager.error(data.message);
+        for (let index = 0; index < this.state.devices.length; index++) {
+          const url = "http://localhost:8081/api/color/"+index+"/"+(color.r)+","+(color.g)+","+(color.b)
+          const response = await fetch(url);
+          const data = await response.json();
+          if (data.code === "200") {
+            NotificationManager.success("Device updated");
+            this.setState({timer: 0});
+          } else {
+            NotificationManager.error(data.message);
+          }
         }
       }
 
@@ -81,7 +89,7 @@ class FetchApi extends React.Component {
             return <div>loading...</div>;
           }
           
-          if (!this.state[0]) {
+          if (!this.state.devices[0]) {
             return <div>failed to collect device state</div>;
           }
 
@@ -89,31 +97,28 @@ class FetchApi extends React.Component {
             <div className="container">
               <div className="row">
                 <p className="last-refresh"><img className="grow" src={refresh} height="40" width="40" onClick={this.fetchState.bind(this)} alt="switch"></img>Last refreshed {this.state.timer} seconds ago.</p>
-
               </div>
-              <div className="row">
-
-              <div className="device-box col-md-3">              
-                  <div className="device-name">All Devices</div>
+              <div className="row">       
+                <div className="device-box col-md-3">
+                  <div className="device-name">Color Palette</div>
                   <CirclePicker 
-                  onChange={ this.handleChange }
-                  />    
-                  <img className="device-switch grow" onClick={() => this.updateState('all')} src={this.state[0].state ? on : off} alt="switch"/>                                
+                  onChange={ this.handleChange }     
+                  />                       
                 </div>
-
+                <div className="device-box col-md-3">
+                  <div className="device-name">All Devices</div>
+                  <img className="device-switch grow" onClick={() => this.updateState('all')} src={this.state.devices[0].device.on ? on : off} alt="switch"/>                           
+                </div>
                 <div className="device-box col-md-3">              
-                  <div className="device-name">{this.state[0].name}</div>   
-                  <img className="device-switch grow" onClick={() => this.updateState(0)} src={this.state[0].state ? on : off} alt="switch"/>
-                                
+                  <div className="device-name">{this.state.devices[0].mode}</div>   
+                  <img className="device-switch grow" onClick={() => this.updateState(0)} src={this.state.devices[0].device.on ? on : off} alt="switch"/>      
                 </div>
-
                 <div className="device-box col-md-3">              
-                  <div className="device-name">{this.state[1].name}</div>
-                  <img className="device-switch grow" onClick={() => this.updateState(1)} src={this.state[1].state ? on : off} alt="switch"/>     
+                  <div className="device-name">{this.state.devices[0].mode}</div>
+                  <img className="device-switch grow" onClick={() => this.updateState(1)} src={this.state.devices[1].device.on  ? on : off} alt="switch"/>     
                 </div>
-
-              </div>
             </div>
+          </div>
 
           );
         }
